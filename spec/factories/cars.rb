024d1c54f389f5622f0f651840be55602e4ba1e7
Sam Gladstone <gladston@umich.edu>
2014-06-29 15:17:21 -0400

    Add test for viewing index page

--- spec/factories/cars.rb
+++ spec/factories/cars.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :car do
     color 'Black'
-    year '1950'
+    sequence(:year) { |n| "195#{n}" }
     sequence(:mileage) { |n| "#{n}" }
     manufacturer
-    description "This car number #{n}."
+    sequence(:description) { |n| "This is car number #{n}." }
   end
 end
---
+++ spec/features/view_cars_index_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+feature 'user views the cars index page', %Q(
+  'As a car salesperson
+  I want to view a list of cars in my lot
+  So that I can keeep track of my cars'
+) do
+
+  scenario 'user views the cars index page' do
+    cars = []
+    5.times do
+      cars << FactoryGirl.create(:car)
+    end
+
+    visit cars_path
+    cars.each do |car|
+      expect(page).to have_content(car.name)
+      expect(page).to have_content(car.color)
+      expect(page).to have_content(car.year)
+      expect(page).to have_content(car.mileage)
+      expect(page).to have_content(car.description)
+    end
+  end
+
+end
