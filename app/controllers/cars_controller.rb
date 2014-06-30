753a824690b937c325cf1849c786de0959d262b0
Sam Gladstone <gladston@umich.edu>
2014-06-30 09:13:37 -0400

    User can add a new car

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -2,4 +2,35 @@ class CarsController < ApplicationController
   def index
     @cars = Car.all.order('created_at DESC')
   end
+
+  def create
+    @car = Car.new(car_params)
+    @manufacturers = Manufacturer.all
+    if !params[:car][:manufacturer_id].nil?
+      @manufacturer = Manufacturer.find(params[:car][:manufacturer_id])
+    else
+      @manufacturer = nil
+    end
+    @car.manufacturer = @manufacturer
+
+    if @car.save
+      flash[:notice] = 'Success! Car saved.'
+      redirect_to cars_path
+    else
+      flash[:notice] = 'Oops! Check your error messages below.'
+      render :new
+    end
+  end
+
+  def new
+    @car = Car.new
+    @manufacturers = Manufacturer.all
+  end
+
+  private
+
+  def car_params
+    params.require(:car).permit(
+    :manufacturer, :color, :year, :mileage, :description)
+  end
 end
--- spec/features/add_car_spec.rb
+++ spec/features/add_car_spec.rb
@@ -7,14 +7,15 @@ feature 'user adds a new car', %Q(
 ) do
 
   scenario 'user successfully adds a new car with a description' do
-    car = FactoryGirl.build(:car)
+    manufacturer = FactoryGirl.create(:manufacturer)
+    car = FactoryGirl.build(:car, manufacturer: manufacturer)
 
     visit new_car_path
-    fill_in 'Manufacturer', with: car.manufacturer.name
+    choose "car_manufacturer_id_#{car.manufacturer.id}"
     fill_in 'Color', with: car.color
     fill_in 'Year', with: car.year
     fill_in 'Mileage', with: car.mileage
-    fill_in 'Description', with: description
+    fill_in 'Description', with: car.description
     click_on 'Submit'
 
     expect(page).to have_content('Success! Car saved.')
@@ -30,7 +31,7 @@ feature 'user adds a new car', %Q(
     car = FactoryGirl.build(:car)
 
     visit new_car_path
-    fill_in 'Manufacturer', with: car.name.manufacturer
+    choose "car_manufacturer_id_#{car.manufacturer.id}"
     fill_in 'Color', with: car.color
     fill_in 'Year', with: car.year
     fill_in 'Mileage', with: car.mileage
@@ -46,7 +47,7 @@ feature 'user adds a new car', %Q(
 
   scenario 'user fails to enter required information' do
     error_msgs = []
-    errors_msgs << 'Oops! Check your error messages below.'
+    error_msgs << 'Oops! Check your error messages below.'
     error_msgs << 'Manufacturer can\'t be blank'
     error_msgs << 'Color can\'t be blank'
     error_msgs << 'Year can\'t be blank'
@@ -55,11 +56,35 @@ feature 'user adds a new car', %Q(
     visit new_car_path
     click_on 'Submit'
 
-    errors.each do |error|
+    error_msgs.each do |error|
       expect(page).to have_content(error)
     end
     expect(page).to have_content('Oops! Check your error messages below')
     expect(page).to_not have_content('Success! Car saved.')
     expect(Car.count).to eq(0)
   end
+
+  scenario 'user enters inappropriate information' do
+    car = FactoryGirl.build(:car)
+
+    error_msgs = []
+    error_msgs << 'Oops! Check your error messages below.'
+    error_msgs << 'Year must be greater than or equal to 1920'
+    error_msgs << 'Mileage must be greater than or equal to 0'
+
+    visit new_car_path
+    choose "car_manufacturer_id_#{car.manufacturer.id}"
+    fill_in 'Color', with: car.color
+    fill_in 'Year', with: 1919
+    fill_in 'Mileage', with: -1
+    click_on 'Submit'
+
+    error_msgs.each do |error|
+      expect(page).to have_content(error)
+    end
+    expect(page).to have_content('Oops! Check your error messages below')
+    expect(page).to_not have_content('Success! Car saved.')
+    expect(Car.count).to eq(0)
+  end
+
 end
